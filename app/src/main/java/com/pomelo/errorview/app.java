3fad869bc1677e708e5bba13917c34ff7253981a
pomelo <1176510444@qq.com>
2017-08-28 17:57:31 +0800

    将VaryViewUtil 移到lib中

---
+++ app/src/main/java/com/pomelo/errorview/app.java
@@ -0,0 +1,33 @@
+package com.pomelo.errorview;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.plmelo.varyview.VaryViewUtil;
+
+
+/**
+ * Created by Administrator on 2017/8/28.
+ */
+
+public class app extends Application {
+
+    private String TAG = "application";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: 执行了");
+
+//        /**
+//         * 可以自定义 空界面 ， 错误界面 ，加载界面
+//         */
+        VaryViewUtil
+                .ViewBuilder
+                .Builder()
+                .setEmptyViewId(R.layout.layout_emptyview)
+                .setErrorViewId(R.layout.layout_errorview)
+                .setLoadingViewId(R.layout.layout_loadingview);
+
+    }
+}
